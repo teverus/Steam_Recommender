b266ce3a376468cd1f5af2debfd414868864f0d7
teverus <teverus@yandex.ru>
2022-07-17 08:00:46 +0300

    pagination | 6

--- Code/tables/TagsTable.py
+++ Code/tables/TagsTable.py
@@ -22,14 +22,22 @@ class TagsTable(Table):
         rows = get_rows(self, tags)
 
         x, y = main.current_position
-        main.current_position = [len(rows) - 1, y] if x > (len(rows) - 1) else [x, y]
 
         # TODO ! нельзя выйти за рамки вперед
         # TODO !! нельзя выйти за рамки назад
-        # TODO !!! припереходе на предыдущую страницу появляется хайлайт
 
-        if main.page_delta == 1 and y == self.max_columns:
+        going_forward = main.page_delta == 1 and y == self.max_columns
+        going_backward = main.page_delta == -1 and y < 0
+        is_in_expected_row_number = x <= (len(rows) - 1)
+
+        if going_forward and is_in_expected_row_number:
             main.current_position = [x, 0]
+
+        elif going_forward and not is_in_expected_row_number:
+            main.current_position = [len(rows) - 1, 0]
+
+        elif going_backward:
+            main.current_position = [x, self.max_columns - 1]
 
         super(TagsTable, self).__init__(
             table_title="Games in Steam by tags  ",
