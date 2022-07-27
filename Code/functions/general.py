2919ad9dcef4dbe01edcd35ab31495f5969d5550
teverus <teverus@yandex.ru>
2022-07-27 15:23:46 +0300

    new concept | 24

--- Code/functions/general.py
+++ Code/functions/general.py
@@ -2,7 +2,15 @@ import msvcrt
 
 from pandas import DataFrame
 
-from Code.constants import TAGS, TAGS_COLUMNS, FILES, Key
+from Code.constants import (
+    TAGS,
+    TAGS_COLUMNS,
+    FILES,
+    Key,
+    SCREEN_WIDTH,
+    HIGHLIGHT,
+    END_HIGHLIGHT,
+)
 from Code.functions.db import read_a_table, append_to_table
 
 
@@ -35,3 +43,12 @@ def wait_for_key(target_key: Key):
     key = msvcrt.getch()
     while key != target_key:
         key = msvcrt.getch()
+
+
+def show_message(message, border="-", centered=True):
+    print(HIGHLIGHT)
+    print(f"{border* SCREEN_WIDTH}")
+    text = message.center if centered else message.ljust
+    print(text(SCREEN_WIDTH))
+    print(f"{border * SCREEN_WIDTH}")
+    print(END_HIGHLIGHT)
--- Code/modules/PerformActionsWithATag.py
+++ Code/modules/PerformActionsWithATag.py
@@ -3,7 +3,7 @@ from Code.Screen import Screen
 from Code.Table import Table
 from Code.constants import TAGS, FILES, Key
 from Code.functions.db import update_a_table
-from Code.functions.general import do_nothing, wait_for_key
+from Code.functions.general import do_nothing, wait_for_key, show_message
 
 
 class PerformActionsWithATag(Screen):
@@ -30,6 +30,7 @@ class PerformActionsWithATag(Screen):
     @staticmethod
     def make_favorite(name):
         update_a_table("Tag", name, "Favorite", 1, TAGS, FILES)
-        # TODO красивое сообщение
-        print('\n The tag is now favorite. Press "Enter" to continue...')
+
+        show_message('The tag is now favorite. Press "Enter" to continue...')
+
         wait_for_key(Key.ENTER)
